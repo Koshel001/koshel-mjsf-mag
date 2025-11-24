85ed58ba42ecf2f203222e9bac3982b5ae413312
Koshel001 <kirillkoshel0108@gmail.com>
2025-11-24 16:15:56 +0200

    Vue.js lab 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,42 +2,26 @@
 import { ref } from 'vue'
 
 defineProps({
-  msg: String,
 })
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="center">
+    <h1>Інженерія програмного забезпечення</h1>
+    <h1>4 курс</h1>
+    <h1>Група 8.1214</h1>
+    <h1>Кошель Кіріл Андрійович</h1>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.center {
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  font-size: 16px;
+  color: #3b3b5c;
 }
 </style>
